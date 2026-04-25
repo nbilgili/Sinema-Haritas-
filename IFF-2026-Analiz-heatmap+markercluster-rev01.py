e6a22e8c93c5a618537ad2d7eae29112db2a4db3
nbilgili <nazirebilgili@yahoo.com>
2026-04-25 22:47:22 +0300

    Update IFF-2026-Analiz-heatmap+markercluster-rev01.py

--- IFF-2026-Analiz-heatmap+markercluster-rev01.py
+++ IFF-2026-Analiz-heatmap+markercluster-rev01.py
@@ -42,9 +42,9 @@ coords={
 
 def build_map(df, coords, start=(41.014, 29.007), zoom=12):
 
-    harita = folium.Map(location=start, zoom_start=zoom, tiles="CartoDB Voyager")
+    harita = folium.Map(location=start, zoom_start=zoom, tiles="CartoDB Voyager", control_scale=True)
 
-    marker_cluster = MarkerCluster().add_to(harita)
+    marker_cluster = MarkerCluster(options={"disableClusteringAtZoom": 13}).add_to(harita)
 
     heat_data = []
 
