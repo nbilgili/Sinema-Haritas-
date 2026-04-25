993ab758dfb14379b2014adf435d5e0565711197
nbilgili <nazirebilgili@yahoo.com>
2026-04-25 23:02:48 +0300

    Update IFF-2026-Analiz-heatmap+markercluster-rev01.py

--- IFF-2026-Analiz-heatmap+markercluster-rev01.py
+++ IFF-2026-Analiz-heatmap+markercluster-rev01.py
@@ -40,7 +40,7 @@ coords={
 # In[5]:
 
 
-def build_map(df, coords, start=(40.98839794899813, 29.02879876774457), zoom=12):
+def build_map(df, coords, start=(40.96955518790454, 29.038563907036295), zoom=12):
 
     harita = folium.Map(location=start, zoom_start=zoom, tiles="CartoDB Voyager", control_scale=True)
 
